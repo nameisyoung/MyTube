6ead9e6f06bfbab0c98b7db03191325e7c4aa9f1
mohagagg <moha.gagg@gmail.com>
2016-05-15 19:22:13 -0700

    Got Tabs set up

---
+++ app/src/main/java/com/nameisyoung/lab3/mytube/SearchFragment.java
@@ -0,0 +1,31 @@
+package com.nameisyoung.lab3.mytube;
+
+/**
+ * Created by Aqeel on 5/15/16.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SearchFragment extends Fragment{
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+}
